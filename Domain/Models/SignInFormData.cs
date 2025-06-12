22c2677538f47f862d682f8d3a36dfe0ce5ed151
4nj4K <anja-kuzmanovic@hotmail.com>
2025-06-12 20:50:55 +0200

    Add files via upload

---
+++ Domain/Models/SignInFormData.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Domain.Models;
+
+public class SignInFormData
+{
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public bool IsPersistent { get; set; }
+}
---
+++ Domain/Models/Status.cs
@@ -0,0 +1,8 @@
+namespace Domain.Models;
+
+public class Status
+{
+    public int Id { get; set; }
+    public string StatusName { get; set; } = null!;
+
+}
