4c2c68907cea82405920e6bc7282c8bb7395aa9e
4nj4K <anja-kuzmanovic@hotmail.com>
2025-06-12 20:46:46 +0200

    Add files via upload

---
+++ Business/Models/StatusResult.cs
@@ -0,0 +1,14 @@
+using Domain.Models;
+
+namespace Business.Models;
+
+public class StatusResult<T> : ServiceResult
+{
+    public T? Result { get; set; }
+}
+
+public class StatusResult : ServiceResult
+{
+
+}
+
---
+++ Business/Models/UserResult.cs
@@ -0,0 +1,9 @@
+using Domain.Models;
+
+namespace Business.Models;
+
+public class UserResult : ServiceResult
+{
+    public IEnumerable<User>? Result { get; set; }
+}
+
